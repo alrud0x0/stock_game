e7b34f50dc75f07e0568209ab879bc2faf22372d
alrud0x0 <alrud0x0@gmail.com>
2020-02-05 17:48:01 +0900

    [#15] 디렉토리 분리

---
+++ front/src/App.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div id="app">
+    <div class="container-scroller">
+      <topTab/>
+    </div>
+    <b-container fluid>
+      <router-view></router-view>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+import TopTab from './components/common/TopTab.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    TopTab,
+  }
+}
+</script>
+
+<style>
+  @import "assets/css/main.css";
+</style>
